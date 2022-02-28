a3ab437a2646b3f6ffd9825b56d529a08b3df93e
tcolombiano <82493225+tcolombiano@users.noreply.github.com>
2022-02-28 15:42:29 -0300

    Aula 12 operações matemáticas
    Operações básicas

---
+++ operacoesmatematicas.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+    //Operações Matemáticas
+    
+    //Definindo variáveis
+    int a =6, b=3;
+    
+    //Soma
+    printf("\nA soma de a e b = %d", a+b);
+    printf("\nA soma de %d e %d é = %d",a,b,a+b);//outra maneira de mostrar mas usando as variáveis já armazenadas
+    
+    //Subtração
+    printf("\nA subtração de a e b = %d", a-b);
+    printf("\nA subtração de %d menos %d é = %d",a,b,a-b);//outra maneira de mostrar mas usando as variáveis já armazenadas
+    //Multiplicação
+    printf("\nA multiplicação de a e b = %d", a*b);
+    printf("\nA multiplicação de %d e %d é = %d",a,b,a*b);//outra maneira de mostrar mas usando as variáveis já armazenadas
+    //Resto da divisão
+    printf("\nO resto da divisão entre de a e b = %d", a%b);
+    printf("\nO resto da divisão de %d entre %d é = %d",a,b,a%b);//outra maneira de mostrar mas usando as variáveis já armazenadas
+    //Valor absoluto- é sempre o valor de um número positivo
+    printf("\nO valor absoluto de -3 = %d", abs(-3));
+    printf("\nO valor absoluto de -%d = %d",a,abs(a));
+
+    
+
+    
+    
+}
