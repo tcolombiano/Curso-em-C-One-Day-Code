5b7de2774bfb5716067e76d33a9c09f00b50f923
tcolombiano <82493225+tcolombiano@users.noreply.github.com>
2022-03-02 09:36:39 -0300

    Comandos básicos para iniciar programas

---
+++ tostart.c
@@ -0,0 +1,8 @@
+//Uso esses comandos abaixo em C para selecionar as bibliotecas básicas que preciso e depois começar a programar.
+#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+
+void main(){
+    setlocale(LC_ALL,"");// adicionando acentuação aos programas
+    //Definindo variáveis
